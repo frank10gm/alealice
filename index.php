2d4f419bbe9d48b0b79845da75cae770691ef383
francesco.laplaca <frank10gm@gmail.com>
2024-02-10 13:07:54 +0100

    basic website

---
+++ index.php
@@ -0,0 +1,316 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Pietro e Giulia 2016">
+    <meta property="og:image" content="" />
+    <meta property="og:title" content="Pietro e Giulia 2016" />
+    <meta property="og:description" content="Pietro e Giulia 2016" />
+    <meta property="og:site_name" content="Pietro e Giulia 2016" />
+    <meta property="og:type" content="website" />
+
+    <title>Pietro e Giulia 2016</title>
+
+    <!-- icona sito -->
+    <link rel="icon" type="image/png" href="img/logo/fav.png">
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+    <!-- FancyBox -->
+    <link rel="stylesheet" type="text/css" href="js/fancybox/source/jquery.fancybox.css?v=2.1.5" media="screen" />
+
+    <!-- LightBox -->
+    <link href="./js/lightbox/css/lightbox.css" rel="stylesheet">
+
+    <!--CSS include-->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- CSS fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+
+<body onresize="" onload="">
+
+    <!-- Navbar
+================================================== -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="nav-back">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+
+                    <ul class="nav navbar-nav navbar-center">
+                        <!-- <li><a class="nav-link" href="#bio-cerimonia">Cerimonia</a></li>
+                        <li><a class="nav-link" href="#bio-ricevimento">Festa</a></li>
+                        <li><a class="nav-link" href="#bio-lista">Lista nozze</a></li>
+                        <li><a class="nav-link" href="#bio-conferma">Conferma</a></li> -->
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- /.navbar -->
+
+
+    <!-- HOME
+================================================== -->
+    <div id="bio-top" class="container spazio">
+        <div class="inner">
+            <div class="copy">
+                <h1>Alice e Alessandro</h1>
+                <p>Domenica 23 giugno 2024</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- partecipazione
+================================================== -->
+    <!-- <div class="container spazio" id="bio-partecipazione">
+        <div class="inner-accentra centra">
+            <div class="accentra">
+                <p class="lead">
+                    “Simone di Giovanni mi ami tu più di costoro?”
+                    <br>
+                    Gesù mendicava il suo amore, mendicava ciò di cui aveva bisogno. Con le lacrime agli occhi Simone disse così forte da spaventare quasi se stesso “Signore, tu sai tutto; tu sai che ti amo”. “Pasci le mie pecorelle” Pietro non avrebbe più avuto che una sola missione da compiere: quella di amare Gesù Cristo, di rispondere alla sua sete di amore, e di rispondervi da peccatore qual era, misero com’era.
+                    <br><br>
+                    <span class="text-muted">Simone chiamato Pietro</span><br>
+                    M. Giuseppe Lepori
+                </p>
+            </div>
+        </div>
+    </div> -->
+
+
+    <!-- cerimonia
+================================================== -->
+    <!-- <div class="container spazio" id="bio-cerimonia">
+
+        <div class="row featurette" style="">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">Cerimonia <span class="text-muted"></span></h2>
+                <br>
+                <p class="lead">
+                    La cerimonia avrà inizio alle ore <span class="text-muted">16:00</span>
+                    <br>
+                    presso la <span class="text-muted">Chiesa di San Domenico</span>
+                    <br>
+                    P.tta Bazzocchi 57
+                    <br>
+                    Cesena
+                </p>
+
+                <a href="https://goo.gl/maps/qsYqTzSEkBP2" target="_blank">Link Google Maps</a>
+            </div>
+            <div class="col-md-5">
+                <br>
+                <br>
+                <br>
+                <img class="featurette-image img-responsive center-block max-foto" src="img/sando.jpg" alt="Generic placeholder image">
+            </div>
+        </div>
+
+    </div> -->
+
+
+    <!-- ricevimento
+================================================== -->
+    <!-- <div class="container spazio" align="center" id="bio-ricevimento" style="">
+
+        <div class="row featurette">
+            <div class="col-md-7 col-md-push-5">
+                <h2 class="featurette-heading">Festa <span class="text-muted"></span></h2>
+                <p class="lead">Dopo la cerimonio siamo felici di festeggiare con tutti voi presso
+                    <span class="text-muted">"Il Cantiere Artistico"</span><br>
+                    EX MIR MAR
+                    Via Roma 27, San Mauro Pascoli (FC)
+                    <br><a href="https://goo.gl/maps/9BQbB42xCQy" target="_blank"><span style="font-size: 12px;">Link Google Maps</span></a>
+                    <br>
+                    <br>
+
+                    <a href="http://ilcantiereartistico.com/" target="_blank">ilcantiereartistico.com</a><br><a href="https://www.facebook.com/ilcantiereartistico/timeline" target="_blank">Facebook</a>
+                </p>
+            </div>
+            <div class="col-md-5 col-md-pull-7">
+                <br>
+                <br>
+                <br>
+                <img class="featurette-image img-responsive center-block max-foto2" src="img/cantiere2.png" alt="Generic placeholder image">
+            </div>
+        </div>
+    </div> -->
+
+    <!-- lista nozze
+================================================== -->
+    <!-- <div id="bio-lista" class="container spazio" align="center">
+        <a href="http://dreamsteam.listanozzeviaggi.it" target="_blank">
+            <h1>Lista <span class="text-muted">nozze</span></h1>
+        </a>
+
+        <p class="lead">
+            Per tutti coloro che desiderano farci un regalo vi chiediamo di contribuire al viaggio di nozze.
+            Al seguente link potrete trovare nel dettaglio le modalità di aiuto e il viaggio che faremo:
+            <br>
+            <br>
+            <a href="http://dreamsteam.listanozzeviaggi.it" target="_blank">listanozzeviaggi.it</a>
+            <br>
+            <span style="font-size: 14px;">
+                username: giuliapietro
+                <br>
+                password: dreamsteam
+                <br>
+                <br>
+            </span>
+            Per chiunque volesse aiutarci a finire di costruire la nostra nuova casa, abbiamo aperto una lista nozze presso la “Comet” di Cesena e Bologna.
+            <br>
+            <br>
+            E’ possibile effettuare anche un bonifico libero al seguente Iban intestato a Biondini Pietro<br>
+            <span class="text-muted">IT68I0200802432000103570147</span>
+        </p>
+
+    </div> -->
+
+
+    <!-- contatti e conferma
+================================================== -->
+    <!-- <div class="container spazio" align="center" id="bio-conferma">
+        <div class="inner-accentra centra">
+            <div class="accentra">
+                <p class="lead">
+                <h2>Contatti e <span class="text-muted">conferma</span></h2>
+                <br>
+                mail: <a href="mailto: matrimonio@pietroegiulia.it">matrimonio@pietroegiulia.it</a><br>
+                cell: Pietro <span class="text-muted">3453466509</span> <span style="color: red;"><br></span> Giulia <span class="text-muted">3273836651</span><br>
+                instagram: <a href="http://www.instagram.com" target="_blank">pietroegiulia</a><br>
+            </div>
+        </div>
+    </div> -->
+
+    <!-- Footer
+================================================== -->
+    <div class="blog-footer">
+        <footer>
+
+            <!--<p class="pull-right">Pietro <span style="color: #585858;">e</span> Giulia  &middot; <a href="#"></a> &middot; <a href="#"></a> &middot; <span style="color: #585858;">2016</span></p>-->
+
+        </footer>
+    </div>
+    <!-- /.footer -->
+
+
+    <!-- JavaScript
+================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery/jquery.min.js"></script>
+    <script src="js/jquery/jquery.easing.min.js"></script>
+
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.file-input.js"></script>
+    <script src="js/holder/holder.js"></script>
+    <script src="./js/lightbox/js/lightbox.min.js"></script>
+    <!--<script src="js/mixitup/jquery.mixitup.min.js"></script>-->
+    <script src="js/masonry/masonry.pkgd.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+
+    <script src="js/custom.js"></script>
+    <script src="js/general.js"></script>
+    <script src="js/loadPage.js"></script>
+
+    <!-- Add fancyBox main JS and CSS files -->
+    <script type="text/javascript" src="js/fancybox/source/jquery.fancybox.js?v=2.1.5"></script>
+
+    <script>
+        /*$('#mostre').load('include/pages/mostre.php');
+
+            $( document ).ready(function() {
+                $('.fancybox').fancybox();
+            });
+
+            $(window).load(function() {
+                //isotope
+                $container = $('#mixitup');
+
+                $container.isotope({
+                    itemSelector: '.mix',
+                    masonry: {
+                        isFitWidth: true,
+                        gutter: 10
+                    }
+                });
+
+                $('#btn-tutto').bind('click', function(event) {
+                    $container.isotope({ filter: '*' });
+                });
+
+                $('#btn-giovedi').bind('click', function(event) {
+                    $container.isotope({ filter: '.giovedi' });
+                });
+
+                $('#btn-venerdi').bind('click', function(event) {
+                    $container.isotope({ filter: '.venerdi' });
+                });
+
+                $('#btn-sabato').bind('click', function(event) {
+                    $container.isotope({ filter: '.sabato' });
+                });
+                $('#btn-domenica').bind('click', function(event) {
+                    $container.isotope({ filter: '.domenica' });
+                    //$container.isotope('reloadItems');
+                });
+
+                $container.isotope('reloadItems');
+                $container.isotope({ filter: '*' });
+            });
+
+
+            //collegamento ai bottoni delle pagine
+            $('#concorso-btn').bind('click', function(event) {
+                $('#inner-concorso').load('include/concorsoFoto.php');
+            });
+
+            $('#form2').submit(function(event){
+                var data = $(this).serialize();
+                $.post('php/elabora_login.php', data)
+                    .success(function(result){
+                    $('#risultato-form').html(result);
+                })
+                    .error(function(){
+                    console.log('Error loading page');
+                })
+                return false;
+            });
+
+            //variabili globali
+            timeout = 0;
+            pagina = '';
+            utente = '';
+            potere = 0;
+            avatar = 'avatar_base.png';
+            */
+    </script>
+
+</body>
+
+</html>
+
+<?php
+
+?>
